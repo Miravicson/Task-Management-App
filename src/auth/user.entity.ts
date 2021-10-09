fc5c4bddf8739e466fa6b21703109bcf35ded2b7
miravicson <victorughonu95@gmail.com>
2021-10-09 07:01:03 +0100

    completed auth module and repository setup

---
+++ src/auth/user.entity.ts
@@ -0,0 +1,13 @@
+import { PrimaryGeneratedColumn, Column, Entity } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  password: string;
+}
